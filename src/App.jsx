d7229e4e71a3b02903c3b29b0b68a1ddae446d17
MrKazio <115210775+Dzban0@users.noreply.github.com>
2025-05-08 17:05:39 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -15,41 +15,6 @@ function App() {
             urlPattern: "/",
             element: <Home />,
         },
-        {
-            id: 2,
-            label: "Laboratorium 1",
-            url: "/lab1",
-            urlPattern: "/lab1",
-            element: <Lab1 />,
-        },
-        {
-            id: 3,
-            label: "Laboratorium 2",
-            url: "/lab2/:id",
-            urlPattern: "/lab2/:id",
-            element: <Lab2 />,
-        },
-        {
-            id: 4,
-            label: "Laboratorium 3",
-            url: "/lab3",
-            urlPattern: "/lab3",
-            element: <Lab3Page />,
-        },
-        {
-            id: 5,
-            label: "Add Person",
-            url: "/lab4/add",
-            urlPattern: "/lab4/add",
-            element: <AddForm />,
-        },
-        {
-            id: 6,
-            label: "Edit Person",
-            url: "/lab4/edit/:id",
-            urlPattern: "/lab4/edit/:id",
-            element: <EditForm />,
-        },
     ];
 
     return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,6 @@
-// import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
-import ShipmentForm from '../components/ShipmentForm';
-import ShipmentTable from '../components/ShipmentTable';
-import Tracking from '../components/Tracking';
-
-export default function HomePage() {
-  const [shipments, setShipments] = useState([]);
-  const [trackingId, setTrackingId] = useState('');
-
-  const addShipment = (newShipment) => {
-    setShipments([...shipments, newShipment]);
-  };
-
-  return (
-    <div>
-      <Navbar />
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">CargoSmart</h1>
-        <ShipmentForm onAddShipment={addShipment} />
-        <ShipmentTable shipments={shipments} />
-        <Tracking trackingId={trackingId} setTrackingId={setTrackingId} shipments={shipments} />
-      </div>
-    </div>
-  );
+function Home() {
+    return <h1>CargoSmart</h1>;
 }
+
+export default Home;
+
