6a217465fa4505331552698b3353564eb846e8f7
MrKazio <115210775+Dzban0@users.noreply.github.com>
2025-05-19 17:34:42 +0200

    Update

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  const user = UserService.getLoggedUser();
+
+  return (
+    <header className="p-4 bg-gray-800 text-white flex justify-between items-center rounded-md">
+      <div>
+        <span className="font-bold">CargoSmart</span>
+      </div>
+      <div>
+        <span className="font-bold">Zalogowany:</span> {user.firstName}{" "}{user.lastName}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
